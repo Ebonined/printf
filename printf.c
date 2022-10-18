8d9b9291a9e7e40f14bb0bfdc4b620fae3319f0e
Daniel Ebonine <ebonined@gmail.com>
2022-10-18 22:27:12 +0100

    made some crucial correction to the code

--- printf.c
+++ printf.c
@@ -3,36 +3,42 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-
 int _printf(const char *format, ...)
 {
-int count = 0;
-va_list prints;
-va_start (prints, format);
+	int count = 0;
+	va_list prints;
+	va_start(prints, format);
 
-for(int i = 0; format[i] != '/0';)
-{ 
-	if (format[i} != '%')
-        {  
-           _putchar(format[i])
-           i++;
-           count ++;
+	for (int i = 0; format[i] != '/0';)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			i++;
+			count++;
+		}
+		else if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+				_putchar(va_arg(prints, int));
+				break;
+				
+				case 's':
+				_putchar(va_arg(prints, char*));
+				break;
+				
+				case '%':
+				_putchar('%');
+				break;
+				
+				default:
+				break;
+			}
+			i++;
+			count++;
+		}
 	}
-	else if (format[i] == '%')
-	{ switch (format[i + 1])
-	case 'c':
-	_putchar(va_arg(prints, int);
-	break;
-	case 's':
-	-putchar(va_arg(prints, char*);
-	break;
-	case '%':
-	_putchar('%');
-	break;
-	default;
-	break;
-        i++;
-	count++;
-	}
+	return (count);
 }
-return (count);
