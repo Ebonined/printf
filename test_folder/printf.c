fe0f6d44f67b0a8d5357a7f7b7b3575820aa03e0
Daniel Ebonine <ebonined@gmail.com>
2022-10-18 22:13:07 +0100

    adjusted code for corrections and to handle formats for task 1 and 2

--- test_folder/printf.c
+++ test_folder/printf.c
@@ -18,33 +18,44 @@ void p_int(va_list b)
 	int_print(output);
 }
 
-
 void p_char(va_list b)
 {
 	int output = va_arg(b, int);
-	putchar(output);
+	write(1, &output, 1);
+}
+
+void p_mod(va_list b)
+{
+	int output = va_arg(b, int);
+	write(1, &output, 1);
 }
 
 void p_str(va_list b)
 {
+	int i = 0;
 	char *output = va_arg(b, char *);
-	printf("%s", output);
+	while (*(output + i))
+	{
+		write(1, &output[i], 1);
+		i++;
+	}
 }
 
 void main()
 {
-	_printf("char is %c; int is %s", 'D', 20, 90);
-	_printf("i", 90);
-	_printf("s", "School");
+	_printf("char is %c; string is %s; and int is %i; just %%", 'D', "STRING", -10022);
 }
 
 int _printf(const char *format, ...)
 {
 	va_list valist;
+	char nl = '\n';
 	add_in task[] = {
 		{"i", p_int},
+		{"d", p_int},
 		{"c", p_char},
-		{"s", p_str}};
+		{"s", p_str},
+		{"%", p_mod}};
 	int i, j;
 
 	// "cis", 's', 3, "ball"
@@ -61,19 +72,25 @@ int _printf(const char *format, ...)
 			{
 				if (format[i + 1] == *task[j].t)
 				{
-					task[j].func(valist);
+					if (*task[j].t == '%')
+						write(1, &(*task[j].t), 1);
+					else
+						task[j].func(valist);
+					i++;
+					break;
 				}
 			}
-			else if (format[i] == '%' && format[i + 1] == '%')
+			else
 			{
-				/* code */
+				write(1, &format[i], 1);
+				break;
 			}
-			
+
 			j++;
 		}
 		i++;
 	}
-
+	write(1, &nl, 1);
 	va_end(valist);
 }
 
@@ -81,51 +98,57 @@ int int_print(int b)
 {
 	int ten = 10;
 	int pr = b / ten;
-	int out, temp, to, times;
+	int out, i, rem, j, remcp, i2;
 	int tenin = 10;
-
-	times = 0;
+	if (b < 0)
+	{
+		out = '-';
+		write(1, &out, 1);
+		b = -1 * b;
+		pr = b / ten;
+	}
 	if (b < 10)
 	{
 		out = (b + '0');
-		times++;
 		write(1, &out, 1);
+
 		return (0);
 	}
-	temp = 10;
-	while (temp)
+	i = 1;
+	while (pr >= ten)
 	{
-		temp = temp * 10;
-		if (b == temp)
-			break;
-		if (b / temp < 1)
+		pr = pr / ten;
+		tenin = tenin * 10;
+		i++;
+	}
+	out = (pr + '0');
+	write(1, &out, 1);
+	rem = b - (pr * tenin);
+	remcp = rem;
+	if (remcp < 10 && remcp > 0 && i == 1)
+	{
+		j = 1;
+	}
+	else if (remcp == 0)
+	{
+		while (i--)
 		{
-			temp = temp / 10;
-			break;
+			int_print(0);
+		}
+		return (0);
+	}
+	else
+	{
+		j = 1;
+		while (remcp > 10)
+		{
+			remcp = remcp / 10;
+			j++;
+		}
+		for (i2 = 0; i2 < (i - j); i2++)
+		{
+			int_print(0);
 		}
 	}
-	if (temp < b)
-	{
-		while (pr > ten)
-		{
-			pr = pr / ten;
-			tenin = tenin * 10;
-		}
-		out = (pr + '0');
-		to = b - (pr * tenin);
-		write(1, &out, 1);
-	}
-	else if (temp == b)
-	{
-		for (i = 0; i < times; i++)
-		{
-			/* code */
-		}
-		
-		out = (pr + '0');
-		to = b - (pr * tenin);
-		write(1, &out, 1);
-	}
-
-	int_print(to);
+	int_print(rem);
 }
